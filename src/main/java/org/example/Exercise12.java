b56259130f4c7bf9712e1525e3a274c85b030327
ReggieCarter2017 <110728456+ReggieCarter2017@users.noreply.github.com>
2023-05-11 19:24:56 +0300

    Create Exercise12.java

---
+++ src/main/java/org/example/Exercise12.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Exercise12 {
+    public static void main(String[] args) {
+
+
+
+
+        CodeReview cr = new CodeReview();
+        cr.firstCode();
+    }
+}
+
+class Exceptions {
+
+    public void firstException()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a float number: ");
+        String test = sc.nextLine();
+        try {
+            Float res = Float.parseFloat(test);
+            System.out.println(res);
+        } catch (NumberFormatException ex) {
+            System.out.println("Invalid number formation!");
+            firstException();
+        }
+    }
+}
+
+class CodeReview {
+    public void firstCode()
+    {
+        Double[] intArray = {1.4, 2.12, 3.1, 4.9, 54.3, 32.2, 53.1, 54.2, 7.3};
+        try {
+            int d = 0;
+            if (d == 0) throw new ArithmeticException("Can't divide by zero!");
+            else {
+                double catchedRes1 = intArray[8] / d;
+                System.out.println("catchedRes1 = " + catchedRes1);
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("IndexOutOfBoundsException");
+        }
+
+    }
+}
